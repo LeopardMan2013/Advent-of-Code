1c1b5aa6606cf73160c65530b3bb35d6b3b918fc
Hamish MacDonald <hamish.macdonald@neds.com>
2024-10-14 19:42:51 +1000

    2015 part 2

--- 2015/Q1/aoc_2015_1.py
+++ 2015/Q1/aoc_2015_1.py
@@ -4,11 +4,16 @@ with open(
     input_lines = input_f.readlines()
 
 floor = 0
+char_pos = 1  # only needed for part 2
 for line in input_lines:
     for char in line:
         if char == "(":
             floor += 1
         elif char == ")":
             floor -= 1
+        if floor == -1:  # only needed for part 2
+            break  # only needed for part 2
+        char_pos += 1  # only needed for part 2
 
-print(f"The answer is {floor}")
+# print(f"The answer is {floor}")
+print(f"The answer to part 2 is {char_poss}")
